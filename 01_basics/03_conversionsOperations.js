1ceb252062ec88f81767320410e8db8bef585701
PawanReddy3005 <149654152+PawanReddy3005@users.noreply.github.com>
2025-02-12 18:31:10 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionsOperations.js
@@ -0,0 +1,23 @@
+let score = "shivam"
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueinnumber = Number(score)
+//console.log(typeof valueinnumber);
+//console.log(valueinnumber)
+
+// "33" = 33
+//"33abc" = NAN (not a number)
+// true = 1 ; false = 0
+let booleanvalueis = 1;
+let booleanis = Boolean(booleanvalueis);
+//console.log(booleanis);
+
+// if we are writing 1 then we get true 
+// if we are writing 0 we are getting false
+// if we are writing "" then we are getting false
+// if we are writing "reddy" then we are getting true 
+ let somenumber = 27
+ let stringnumber = String(somenumber)
+ console.log(stringnumber)
+ console.log(typeof stringnumber)
