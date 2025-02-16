8ac5c0988f740e3fcef5d96bb1db8dc24ec8477a
PawanReddy3005 <149654152+PawanReddy3005@users.noreply.github.com>
2025-02-16 18:12:10 +0000

    06_maths_and_num.js

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,25 @@
+const score = 400
+// console.log(score)
+
+const newscore = new Number(300)
+// console.log(newscore)
+
+// console.log(newscore.toString().length)
+
+const hundereds = 1000000
+// console.log(hundereds.toLocaleString('en-IN'));
+
+// ++++++++++++++++++++++ maths ++++++++++++++++++++++++++++
+
+
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.556));
+console.log(Math.random()); // it gives random values between 0 and 1 
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()*(max - min + 1)) + min);
+
+
