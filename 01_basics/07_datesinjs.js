d81b352e984677c8ea844224ef4c033bcd02534d
PawanReddy3005 <149654152+PawanReddy3005@users.noreply.github.com>
2025-02-16 20:32:12 +0000

    data in js

---
+++ 01_basics/07_datesinjs.js
@@ -0,0 +1,11 @@
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+
+let newdate = new Date()
+console.log(newdate);
+console.log(newdate.getMonth());
+
+
