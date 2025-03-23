d58704a5249541c8e0c18f4a6d659b1daf37d50d
PawanReddy3005 <149654152+PawanReddy3005@users.noreply.github.com>
2025-03-23 19:21:21 +0000

    loops

---
+++ 05_iterations/four.js
@@ -0,0 +1,13 @@
+const myObject = {
+    js:'javascript',
+    cpp : ' c++',
+    rb : 'Ruby',
+    swift : "swift by apple",
+}
+
+    for (const key in myObject) {
+        // console.log(myObject[key]);
+        console.log(`${key} shortcut is for  ${myObject[key]}`);
+        
+       
+}
