8529bbbcd81f734ff9bf345a3746797807c1f1b7
PawanReddy3005 <149654152+PawanReddy3005@users.noreply.github.com>
2025-03-23 14:30:27 +0000

    while nad do_while loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,13 @@
+// let index =  0
+// while (index <= 10) {
+//     console.log(`value of the index is ${index}`);
+//     index = index + 2;
+// }
+
+let score  = 1
+
+do {
+    console.log(`score is ${score}`);
+    score ++ 
+    
+} while (score <=10);
