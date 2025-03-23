4eccaf54b89de8a944496c7bf6dcbc7fa03aa57c
PawanReddy3005 <149654152+PawanReddy3005@users.noreply.github.com>
2025-03-23 13:51:41 +0000

    truthy and falsy values

--- 04_controlFLow/truthy.js
+++ 04_controlFLow/truthy.js
@@ -1,7 +1,43 @@
-const userEmail = "reddy@gmail.com"
-if (userEmail) {
-    console.log("got the user email");
-} else {
-    console.log("dont hav ethe user email");
+// const userEmail = "reddy@gmail.com"
+// if (userEmail) {
+//     console.log("got the user email");
+// } else {
+//     console.log("dont hav ethe user email");
     
-}
+// }
+
+// falsy values 
+// false , 0, -0, "", undefined ,null ,NaN
+
+// truthy values 
+// "0" (if ther eis 0 in string then it is called truthy value)
+// 'false' , " ", [] , {}, function(){}
+
+// if (userEmail.length === 0) {
+//     console.log("array is empty");
+        
+// }
+
+const emptyobj = {}
+
+// if (Object.keys(emptyobj).length === 0) {
+//     console.log("object is empty");
+       
+// }
+
+// nullish coalescing opertaor (??): null defined 
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+// val1 = null ??10 ?? 15
+
+// console.log(val1);
+
+// terniary opertaors 
+
+// condition ? true  : false
+
+const iceTeaPrice = 100 
+iceTeaPrice <=80 ? console.log("less than 80") : console.log("more than 80");
+
